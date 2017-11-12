fd8724bf2a0725ff75a0cfbf23dff4d2ad92285d
Arya Soni <aryasoni98@gmail.com>
2017-11-12 23:44:53 +0530

    upload

---
+++ login-form-ui-design/js/index.js
@@ -0,0 +1,41 @@
+$(function(){
+  /*------- button with class register -------*/
+  var reg_btn =$('.container .register'),
+  /*------- button with class sign in --------*/
+      sig_btn =$('.container .signin'),
+  /*------- back button ----------------------*/
+      back_btn =$('.container .back');
+  
+  reg_btn.on('click',function(e){
+    e.preventDefault();
+    reg_btn.siblings('.reg').css({
+      'transform':'translateY(40%) scale(5)',
+      'border-radius':'0',
+      'width':'100%',
+      'height':'100%'
+    }).end();
+    
+  
+    reg_btn.siblings('.container h3:nth-of-type(1)').css({
+      'top':'40%',
+      'z-index':'8',
+    }).end().end();   
+  });
+  
+   sig_btn.on('click',function(e){
+    e.preventDefault();
+    sig_btn.siblings('.sig').css({
+      'transform':'translateY(40%) scale(5)',
+      'border-radius':'0',
+      'width':'100%',
+      'height':'100%'
+    }).end();
+     
+ 
+    sig_btn.siblings('.container h3:nth-of-type(2)').css({
+      'top':'40%',
+      'z-index':'8',
+    }).end().end(); 
+    
+  });
+})
